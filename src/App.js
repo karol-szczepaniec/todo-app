02ef1e122a015cd10f12445b6787a2ff5cd0d8f4
charlie_sz <karol.szczepaniec@gopos.pl>
2021-03-07 15:24:06 +0100

    add actions: -add item -remove item -mark / unmark completed item

--- src/App.js
+++ src/App.js
@@ -4,37 +4,58 @@ import TodoItem from "./components/TodoItem"
 
 function App() {
 
-    const [items, dispatch] = useReducer(taskReducer,{todoItems:[
-    //const [items, setItems] = useState({todoItems:[
+    const [items, setItems] = useState({todoItems:[
         {
             id: 1,
-            isCompleted: true,
+            isCompleted: false,
             date: '2021-03-08',
             contentText: 'jakiś tekst',
             assignedPerson: 'id: 11'
         },
         {
             id: 2,
-            isCompleted: false,
+            isCompleted: true,
             date: '2021-03-08',
             contentText: 'opis taska',
             assignedPerson: 'id: 22'
         },
         ]})
 
-    function taskReducer(state, action){
-        return action.payload;
+    function ListActions(action){
+
+        const index = items.todoItems.findIndex(el => el.id == action.payload.id)
+        let newList = items.todoItems.slice();
+
+        switch (action.payload.type){
+            case "ADD":
+                let newId = items.todoItems[items.todoItems.length-1].id ?
+                    items.todoItems[items.todoItems.length-1].id+1 :
+                    items.todoItems.length+1;
+                let newItem = {
+                    id: newId,
+                    isCompleted: false,
+                    date: new Date (Date.now()).toLocaleDateString(),
+                    contentText: 'jakiś task do zrobienia',
+                    assignedPerson: 'id: 22'
+                }
+                newList.push(newItem)
+                break;
+            case "REMOVE":
+                newList = items.todoItems.filter(el => el.id != action.payload.id)
+                break;
+            case 'MARK':
+                newList[index].isCompleted = !items.todoItems[index].isCompleted;
+                break;
+            default:
+                return true;
+        }
+
+        setItems({todoItems: newList});
     }
 
-    function markCompleted(elId){
-        const index = items.todoItems.findIndex(el => el.id == elId);
-        const newItems = items.todoItems;
-        newItems[index].isCompleted = !newItems[index].isCompleted;
-        dispatch({type: "COMPLET", payload: {todoItems:newItems}})
-    }
 
 
-    const todoList = items.todoItems.map(i=><TodoItem key={i.id} item={i} markTask={markCompleted}/>)
+    const todoList = items.todoItems.map(i=><TodoItem key={i.id} item={i} taskActions={ListActions}/>)
 
   return (
     <div className="App">
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 export default function TodoItem(props){
+
     return(
         <div className="item">
             id: {props.item.id} <br/>
@@ -9,10 +10,22 @@ export default function TodoItem(props){
             <p>isCompleted: {props.item.isCompleted ? 'tak' : 'nie'}</p> <br/>
             person: {props.item.assignedPerson} <br/>
             <br/>
+
             <button onClick={(e)=>{
                 e.preventDefault();
-                props.markTask(props.item.id)
-            }}>MarkCompleted</button>
+                props.taskActions({payload:{type: 'ADD',id: props.item.id}})
+            }}>Add item</button>
+
+            <button onClick={(e)=>{
+                e.preventDefault();
+                props.taskActions({payload:{type: 'REMOVE',id: props.item.id}})
+            }}>Remove</button>
+
+            <button onClick={(e)=>{
+                e.preventDefault();
+                props.taskActions({payload:{type: 'MARK',id: props.item.id}})
+            }}>Mark</button>
+
         </div>
     )
 }
